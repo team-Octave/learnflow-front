78ae627d801311446790c21f2106c7c74ac4d35b
seyun0714 <seun0714@naver.com>
2025-12-06 19:25:04 +0900

    feat: footer 구현

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center w-full h-24 bg-zinc-950">
+      <div className="text-center text-sm font-medium text-zinc-400">
+        © 2025 LearnFlow. All rights reserved.
+      </div>
+    </footer>
+  );
+}
