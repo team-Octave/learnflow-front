e9575e4380d06b3f0624cb8db4e394c197905208
seyun0714 <seun0714@naver.com>
2025-12-06 19:23:08 +0900

    style: pretendard 폰트 전역 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import './globals.css';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2', // 폰트 파일 경로 (알맞게 수정하세요)
+  display: 'swap',
+  weight: '45 920', // 가변 폰트 웨이트 범위 설정
+  variable: '--font-pretendard', // CSS 변수 이름 지정
+});
 
 export default function RootLayout({
   children,
@@ -6,8 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="en" className="dark">
+      <body
+        className={`${pretendard.variable} font-sans antialiased min-h-screen bg-background text-foreground`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
