725b014af18247c27668caf353d8762af5b68d3c
seyun0714 <seun0714@naver.com>
2025-12-11 10:09:24 +0900

    feat: 로그인 요청/응답 시 필요한 타입 추가

--- src/features/auth/types.ts
+++ src/features/auth/types.ts
@@ -1,7 +1,32 @@
 export interface User {
   userId: string;
-  userEmail: string;
+  email: string;
   nickname: string;
   role: 'MEMBER' | 'ADMIN';
   createdAt: string;
+}
+
+export interface AuthState {
+  user: Pick<User, 'email' | 'nickname' | 'role'> | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  nickname: string;
+  email: string;
+  role: 'MEMBER' | 'ADMIN';
+}
+
+export interface LoginResponseWithToken {
+  nickname: string;
+  email: string;
+  role: 'MEMBER' | 'ADMIN';
+  accessToken: string;
+  refreshToken: string;
 }
