9fbf322fab10060a6157320ddce60af4a813777d
seyun0714 <seun0714@naver.com>
2025-12-11 10:05:24 +0900

    feat: 서버쪽 로그인 여부 체크 기능 추가(새로고침 로그인 깜빡 현상 해결)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,18 +1,23 @@
 // learnflow-front/src/app/(main)/layout.tsx
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import { cookies } from 'next/headers';
 
 //MainLayout이라는 레이아웃 컴포넌트를 정의.
 //페이지 전체 레이아웃을 잡아주는 컴포넌트.
-export default function MainLayout({
+export default async function MainLayout({
   children, //이 레이아웃 안에 들어가는 실제 페이지 내용.
 }: Readonly<{
   //children을 수정 못 하게 하는 타입 설정(그냥 타입 안정성용).
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  // 리프레시 토큰으로 서버쪽에서 로그인 여부 체크
+  const hasToken = cookieStore.has('refreshToken');
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header serverLoginCheck={hasToken} />
       <main className="flex-1 w-full flex flex-col items-center">
         {/* 실제 각 페이지 내용이 여기 들어감. */}
         {children}
