5b8e898b76b73c6dc29726c968e171ae76b26b97
seyun0714 <seun0714@naver.com>
2025-12-11 10:07:50 +0900

    feat: login, logout 액션 구현

--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -1,83 +1,58 @@
-// src/features/auth/actions.ts
 'use server';
 
-/* ---------------------- 로그인 API ---------------------- */
-export async function loginAction(email: string, password: string) {
+import { login } from '@/services/auth.service';
+import { cookies } from 'next/headers';
+import { LoginRequest, LoginResponse, User } from './types';
+
+type ActionState<T> = {
+  success: boolean;
+  data?: T;
+  error?: string;
+};
+
+export async function loginAction(
+  email: string,
+  password: string,
+): Promise<ActionState<LoginResponse>> {
+  const payload: LoginRequest = { email, password };
+  // auth.service.ts 의 login 함수
   try {
-    const res = await fetch('http://localhost:8080/api/v1/auth/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
+    const body = await login(payload);
+
+    // 쿠키 설정
+    const cookieStore = await cookies();
+
+    // 엑세스 토큰 저장
+    cookieStore.set('accessToken', body.accessToken, {
+      httpOnly: true, // 자바스크립트 접근 불가
+      maxAge: 60 * 60, // 1시간
+      path: '/',
     });
 
-    const data = await res.json();
-    return data; // { success, code, message, data }
+    // 리프레시 토큰 저장
+    cookieStore.set('refreshToken', body.refreshToken, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60, //7일
+      path: '/',
+    });
+    const result = {
+      email: body.email,
+      nickname: body.nickname,
+      role: body.role,
+    };
+
+    return { success: true, data: result };
   } catch (error) {
     return {
       success: false,
-      code: 'NETWORK_ERROR',
-      message: '서버 요청 실패',
-      data: null,
+      error: error instanceof Error ? error.message : '로그인 실패',
     };
   }
 }
 
-/* ---------------------- 닉네임 중복 확인 API ---------------------- */
-export async function checkNicknameAction(nickname: string) {
-  try {
-    const res = await fetch(
-      `http://localhost:8080/api/v1/users/check?nickname=${nickname}`,
-      {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-        cache: 'no-store',
-      },
-    );
-
-    const data = await res.json();
-
-    return {
-      success: data.success,
-      message: data.message,
-      data: data.data, // true = 중복, false = 사용 가능
-    };
-  } catch (error) {
-    return {
-      success: false,
-      message: '닉네임 중복 확인 중 오류가 발생했습니다.',
-      data: null,
-    };
-  }
-}
-
-/* ---------------------- 회원가입 API ---------------------- */
-export async function signupAction(payload: {
-  email: string;
-  password: string;
-  nickname: string;
-}) {
-  try {
-    const res = await fetch('http://localhost:8080/api/v1/users', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(payload),
-      cache: 'no-store',
-    });
-
-    const data = await res.json();
-
-    return {
-      success: data.success,
-      code: data.code,
-      message: data.message,
-      data: data.data,
-    };
-  } catch (error) {
-    return {
-      success: false,
-      code: 'NETWORK_ERROR',
-      message: '서버 요청 실패',
-      data: null,
-    };
-  }
+export async function logoutAction(): Promise<ActionState<null>> {
+  const cookieStore = await cookies();
+  cookieStore.delete('accessToken');
+  cookieStore.delete('refreshToken');
+  return { success: true };
 }
