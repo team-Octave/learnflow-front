9996e4068826227cfeb421d6381e1c14ce11d12f
seyun0714 <seun0714@naver.com>
2025-12-12 18:02:58 +0900

    feat: 내 학습 목록 조회, 리뷰 등록/삭제 api 로직 작성

--- src/services/learning.service.ts
+++ src/services/learning.service.ts
@@ -1,3 +1,43 @@
+'use server';
+
+import { ReviewRequest } from '@/features/learning/types';
+import { authFetch } from '@/lib/api';
+
+export async function getLearningLectures() {
+  const response = await authFetch(`/api/v1/enrollment/my`);
+  const data = await response.json();
+  if (response.ok) {
+    return data;
+  } else {
+    throw new Error(data.message || '강의 목록 조회 실패');
+  }
+}
+
+export async function writeReview(review: ReviewRequest) {
+  const response = await authFetch(`/api/v1/reviews`, {
+    method: 'POST',
+    body: JSON.stringify(review),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    return data;
+  } else {
+    throw new Error(data.message || '리뷰 작성 실패');
+  }
+}
+export async function deleteReview(reviewId: number) {
+  const response = await authFetch(`/api/v1/reviews/${reviewId}`, {
+    method: 'DELETE',
+  });
+  const data = await response.json();
+  console.log(data);
+  if (response.ok) {
+    return data;
+  } else {
+    throw new Error(data.message || '리뷰 삭제 실패');
+  }
+}
+
 /* 내 학습 목록 조회
 내 학습 목록에서 수강 취소
 내 학습 목록에 추가(신규 수강신청)
@@ -10,9 +50,9 @@
 */
 
 // 학습 진행 상태(더미 데이터)
-export function getLearningProgress(lectureId: string) {
-  return {
-    lastCompletedLessonId: 'l-2-1',
-    progressRate: 42,
-  };
-}
+// export function getLearningProgress(lectureId: string) {
+//   return {
+//     lastCompletedLessonId: 'l-2-1',
+//     progressRate: 42,
+//   };
+// }
