2b0a87802108dba83cf7928a60dfd03300a9bb9a
seyun0714 <seun0714@naver.com>
2025-12-13 22:12:17 +0900

    feat: 강의 등록 api 로직 작성

--- src/services/creator.service.ts
+++ src/services/creator.service.ts
@@ -1,3 +1,4 @@
+import { BasicInfo } from '@/features/creator/types';
 import { authFetch } from '@/lib/api';
 
 export async function getCreatorLectures() {
@@ -31,5 +32,31 @@ export async function publishCreatorLecture(id: number) {
     return data;
   } else {
     throw new Error(data.message || '강의 공개 실패');
+  }
+}
+
+export async function createBasicLecture(payload: Omit<BasicInfo, 'file'>) {
+  const response = await authFetch(`/api/v1/lectures`, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    return data;
+  } else {
+    throw new Error(data.message || '강의 기본 정보 등록 실패');
+  }
+}
+
+export async function uploadThumbnail(formData: FormData) {
+  const response = await authFetch(`/api/v1/contents/upload-thumbnail`, {
+    method: 'POST',
+    body: formData,
+  });
+  const data = await response.json();
+  if (response.ok) {
+    return data;
+  } else {
+    throw new Error(data.message || '강의 썸네일 등록 실패');
   }
 }
