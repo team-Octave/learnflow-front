292e04c5fc4b080c795755c104ddd037657eab3b
joyh7680 <joy.h7680@gmail.com>
2025-12-13 22:46:36 +0900

    feat : 유저정보 조회 api 연동

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -1,24 +1,37 @@
+// src/app/(main)/mypage/page.tsx
 import AccountDelete from '@/features/user/components/AccountDelete';
-import AccountDeleteButton from '@/features/user/components/AccountDeleteButton';
 import ProfileInfo from '@/features/user/components/ProfileInfo';
+import { getMe } from '@/services/auth.service';
+import { redirect } from 'next/navigation';
 
-export default function MyPage() {
+interface Me {
+  nickname: string;
+  email: string;
+  role: 'MEMBER' | 'ADMIN';
+}
+
+export default async function MyPage() {
+  let me: Me;
+
+  try {
+    me = await getMe();
+  } catch (e) {
+    redirect('/login');
+  }
+
   return (
     <div className="w-full flex justify-center my-12">
       <div className="w-full max-w-[520px] flex flex-col items-center">
-        {/* 내 정보 제목 */}
         <h1 className="text-2xl font-bold text-white mb-8 w-full px-2">
           내 정보
         </h1>
 
-        {/* 프로필 정보 박스 */}
         <div className="w-full mb-6">
-          <ProfileInfo />
+          <ProfileInfo nickname={me.nickname} email={me.email} />
         </div>
 
-        {/* 회원 탈퇴 박스 */}
         <div className="w-full">
-          <AccountDelete />
+          <AccountDelete email={me.email} />
         </div>
       </div>
     </div>
