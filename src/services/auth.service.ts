15a22533b32ad97e34f68e38110a2dfb63ae01f2
seyun0714 <seun0714@naver.com>
2025-12-27 15:50:05 +0900

    refactor: service의 return type 제거

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,13 +1,8 @@
 // src/services/auth.service.ts
 
-import {
-  LoginRequest,
-  LoginResponseWithToken,
-  User,
-} from '@/features/auth/types';
 import { commonFetch, authFetch } from '@/shared/api'; //  authFetch 추가
 
-async function login(user: LoginRequest): Promise<LoginResponseWithToken> {
+async function login(user: { email: string; password: string }) {
   const response = await commonFetch(`/api/v1/auth/login`, {
     method: 'POST',
     body: JSON.stringify(user),
@@ -15,7 +10,7 @@ async function login(user: LoginRequest): Promise<LoginResponseWithToken> {
   return response.json();
 }
 
-async function getUser(): Promise<Pick<User, 'email' | 'nickname' | 'role'>> {
+async function getUser() {
   const response = await authFetch('/api/v1/users/me', {
     method: 'GET',
   });
