1232710f54269a631e1282b94c9e03b540cf0e1a
seyun0714 <seun0714@naver.com>
2025-12-27 15:17:42 +0900

    refactor: service 레이어 return 형식 통일

--- src/services/learning.service.ts
+++ src/services/learning.service.ts
@@ -1,29 +1,19 @@
 'use server';
 
 import { ReviewRequest } from '@/features/learning/types';
-import { authFetch } from '@/lib/api';
+import { authFetch } from '@/shared/api';
 
 export async function enrollLecture(lectureId: number) {
   const response = await authFetch(`/api/v1/enrollment`, {
     method: 'POST',
     body: JSON.stringify({ lectureId }),
   });
-  const data = await response.json();
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error(data.message || '수강 신청 실패');
-  }
+  return response.json();
 }
 
 export async function getLearningLectures() {
   const response = await authFetch(`/api/v1/enrollment/my`);
-  const data = await response.json();
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error(data.message || '강의 목록 조회 실패');
-  }
+  return response.json();
 }
 
 export async function writeReview(review: ReviewRequest) {
@@ -31,23 +21,13 @@ export async function writeReview(review: ReviewRequest) {
     method: 'POST',
     body: JSON.stringify(review),
   });
-  const data = await response.json();
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error(data.message || '리뷰 작성 실패');
-  }
+  return response.json();
 }
 export async function deleteReview(reviewId: number) {
   const response = await authFetch(`/api/v1/reviews/${reviewId}`, {
     method: 'DELETE',
   });
-  const data = await response.json();
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error(data.message || '리뷰 삭제 실패');
-  }
+  return response.json();
 }
 
 export async function getEnrollmentById(enrollmentId: number) {
@@ -55,12 +35,7 @@ export async function getEnrollmentById(enrollmentId: number) {
     method: 'POST',
     body: JSON.stringify({ enrollmentId }),
   });
-  const data = await response.json();
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error(data.message || '수강 정보 조회 실패');
-  }
+  return response.json();
 }
 
 export async function completeLesson(enrollmentId: number, lessonId: number) {
@@ -69,30 +44,5 @@ export async function completeLesson(enrollmentId: number, lessonId: number) {
     method: 'POST',
     body: JSON.stringify({ enrollmentId, lessonId }),
   });
-  const data = await response.json();
-  console.log(data);
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error(data.message || '수강 완료 처리 실패');
-  }
+  return response.json();
 }
-
-/* 내 학습 목록 조회
-내 학습 목록에서 수강 취소
-내 학습 목록에 추가(신규 수강신청)
-특정 강의 수강 클릭 시 수강 정보 조회
-특정 레슨 완료 등등
-
-수강 페이지도 거기에서 써요
-특정 강의 수강 클릭 시 수강 정보 조회
-이게 수강 페이지에서 요청할 API 
-*/
-
-// 학습 진행 상태(더미 데이터)
-// export function getLearningProgress(lectureId: string) {
-//   return {
-//     lastCompletedLessonId: 'l-2-1',
-//     progressRate: 42,
-//   };
-// }
