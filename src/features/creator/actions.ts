47ac3b3ed1aef19565ab06d747d76630c5fe1c30
seyun0714 <seun0714@naver.com>
2025-12-27 16:02:45 +0900

    refactor: 강의 기본 정보 등록 state만 우선 반환하도록 수정

--- src/features/creator/actions.ts
+++ src/features/creator/actions.ts
@@ -36,7 +36,7 @@ export async function publishCreatorLectureAction(
 // 우선 두 개의 state를 return 하도록 구현
 export async function createBasicLectureAction(
   formData: FormData,
-): Promise<ActionState<any>[]> {
+): Promise<ActionState<any>> {
   const title = formData.get('title') as string;
   const categoryId = formData.get('categoryId') as Category;
   const level = formData.get('level') as Level;
@@ -51,16 +51,16 @@ export async function createBasicLectureAction(
   };
 
   // 강의 기본 정보로 먼저 등록하고, ID를 가져옴
-  const lectureState = await createBasicLecture(payload);
+  const state = await createBasicLecture(payload);
 
-  const lecture = lectureState.data as Lecture;
+  const lecture = state.data as Lecture;
   const thumbnailFormData = new FormData();
   thumbnailFormData.append('lectureId', lecture.id);
   thumbnailFormData.append('file', file);
   // 등록된 ID로 썸네일 등록 API 호출
   const thumbnailState = await uploadThumbnail(thumbnailFormData);
 
-  return [lectureState, thumbnailState];
+  return state;
 }
 
 export async function createCurriculumAction(
