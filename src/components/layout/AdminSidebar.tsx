8f3d14368b4b46b520d401d2bdb6d63fe4b3dfd3
seyun0714 <seun0714@naver.com>
2026-01-09 15:23:35 +0900

    feat: AdminSidebar 컴포넌트 구현

--- src/components/layout/AdminSidebar.tsx
+++ src/components/layout/AdminSidebar.tsx
@@ -1,3 +1,33 @@
+import {
+  BookOpenCheckIcon,
+  LayoutDashboardIcon,
+  MessageSquareWarningIcon,
+  UserXIcon,
+} from 'lucide-react';
+import SidebarMenu from './SidebarMenu';
+
 export default function AdminSidebar() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col w-64 bg-zinc-900 p-4 gap-4 rounded-md border border-zinc-800">
+      <h2 className="text-white text-xl font-bold pl-1">ADMIN</h2>
+      <nav className="flex flex-col gap-2">
+        <SidebarMenu>
+          <LayoutDashboardIcon size={20} />
+          <span>대시보드</span>
+        </SidebarMenu>
+        <SidebarMenu>
+          <UserXIcon size={20} />
+          <span>탈퇴 유저 관리</span>
+        </SidebarMenu>
+        <SidebarMenu href="/admin/audit">
+          <BookOpenCheckIcon size={20} />
+          <span>강의 검토</span>
+        </SidebarMenu>
+        <SidebarMenu>
+          <MessageSquareWarningIcon size={20} />
+          <span>악성 리뷰 관리</span>
+        </SidebarMenu>
+      </nav>
+    </div>
+  );
 }
