7f9ef494c7e3b06a53ef4252c465cb7ccde79296
seyun0714 <seun0714@naver.com>
2026-01-09 15:20:44 +0900

    fix: admin 오타 수정 및 admin layout 설정

--- src/app/(main)/admin/layout.tsx
+++ src/app/(main)/admin/layout.tsx
@@ -1,11 +1,14 @@
+import AdminSidebar from '@/components/layout/AdminSidebar';
+
 export default function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="flex p-10 gap-10 w-full">
       {/* AdminSidebar 자리 */}
+      <AdminSidebar />
       {children}
     </div>
   );
