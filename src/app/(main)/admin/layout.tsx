fa7b40f8a2e94c03a06f7353ba96553b9a334c3d
joyh7680 <joy.h7680@gmail.com>
2026-01-13 13:52:51 +0900

    fix: toaster 위치 수정

--- src/app/(main)/admin/layout.tsx
+++ src/app/(main)/admin/layout.tsx
@@ -1,5 +1,4 @@
 import AdminSidebar from '@/components/layout/AdminSidebar';
-import { Toaster } from '@/components/ui/sonner';
 
 export default function AdminLayout({
   children,
@@ -10,7 +9,6 @@ export default function AdminLayout({
     <div className="flex p-10 gap-10 w-full">
       {/* AdminSidebar 자리 */}
       <AdminSidebar />
-      <Toaster position="top-center" />
       {children}
     </div>
   );
