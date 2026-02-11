efa1388ffaa2736da417aee361175c5d18fc15c9
joyh7680 <joy.h7680@gmail.com>
2026-02-11 18:54:49 +0900

    refactor: 메인페이지 시맨틱태그 수정

--- src/app/(main)/detail/[id]/page.tsx
+++ src/app/(main)/detail/[id]/page.tsx
@@ -51,7 +51,7 @@ export default async function LectureDetailPage({
       <LectureSummary lecture={lecture} />
 
       {/* 메인 컨텐츠 영역 */}
-      <div className="px-4 md:px-8 py-12 flex flex-col md:flex-row gap-12 w-full">
+      <section className="px-4 md:px-8 py-12 flex flex-col md:flex-row gap-12 w-full">
         {/* 왼쪽: 상세 + 탭(커리큘럼 / 리뷰) */}
         <div className="flex-1">
           {/* 탭 영역 */}
@@ -105,7 +105,7 @@ export default async function LectureDetailPage({
             </Tabs>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
